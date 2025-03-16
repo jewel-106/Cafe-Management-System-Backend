e019124cd6d5e26d510e773d7498de6f62c35a31
jewel-106 <mjrcse106@gmail.com>
2025-03-16 17:10:08 +0600

    update

--- connection.js
+++ connection.js
@@ -1,45 +1,22 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
-
-// Create a new pool with the database URL from environment variables
-// const pool = new Pool({
-//     host: 'db.sqmrjdbukuthsimdnviz.supabase.co',
-//     port: 5432,
-//     database: 'postgres',
-//     user: 'postgres',
-//     password: 'testadmin',
-//     ssl: { rejectUnauthorized: false }
-// });
-// console.log("DATABASE_URL", process.env.DATABASE_URL);
-
-
-
-// // Handle connection errors
-// pool.on('connect', () => {
-//     console.log('Connected to the PostgreSQL database');
-// });
-
-// pool.on('error', (err) => {
-//     console.error('Error with PostgreSQL connection:', err);
-// });
 const pool = new Pool({
-    port: process.env.DB_PORT || 5432,
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USERNAME || 'cafe',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'cafe',
-    connectTimeout: 10000
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false, // Allow self-signed certificates
+  },
 });
 
-
-// Establish connection
-pool.connect((err, client, release) => {
-    if (!err) {
-        console.log("✅ Connected to PostgreSQL Successfully");
-    } else {
-        console.error("❌ Database Connection Failed:");
-    }
-});
+pool
+  .connect()
+  .then(() => console.log("✅ PostgreSQL connected successfully"))
+  .catch((err) => console.error("❌ Error connecting to PostgreSQL:", err));
 
 module.exports = pool;
